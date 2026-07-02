4abd8af33709f613d7bf48a7112ecb30a741542e
Sean Davis <downtowncaptures@gmail.com>
2026-07-02 02:31:26 -0700

    Update PluginMain.cpp

--- src/PluginMain.cpp
+++ src/PluginMain.cpp
@@ -2,16 +2,13 @@
 
 CReqQueuePlugin* g_pMyPlugin = NULL;
 
-extern "C"
+void __declspec(dllexport) EuroScopePlugInInit(EuroScopePlugIn::CPlugIn** ppPlugInInstance)
 {
-    void __declspec(dllexport) EuroScopePlugInInit(EuroScopePlugIn::CPlugIn** ppPlugInInstance)
-    {
-        *ppPlugInInstance = g_pMyPlugin = new CReqQueuePlugin();
-    }
+    *ppPlugInInstance = g_pMyPlugin = new CReqQueuePlugin();
+}
 
-    void __declspec(dllexport) EuroScopePlugInExit(void)
-    {
-        delete g_pMyPlugin;
-        g_pMyPlugin = NULL;
-    }
+void __declspec(dllexport) EuroScopePlugInExit(void)
+{
+    delete g_pMyPlugin;
+    g_pMyPlugin = NULL;
 }
